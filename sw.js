7deda85b2bd740b600d4d614edf624ed8c195e3d
webhive141508-netizen <webhive141508@gmail.com>
2025-12-13 03:27:58 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-// Minimal Service Worker v7
+// Minimal Service Worker v8
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-
-// Just pass through all requests - no caching interference
 self.addEventListener('fetch', () => {});
