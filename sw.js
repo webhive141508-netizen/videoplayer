460e31689719c8087d662d8852ce29c930063db3
webhive141508-netizen <webhive141508@gmail.com>
2025-12-13 12:17:44 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,3 @@
-// Service Worker - Network Only (No Caching)
-// This ensures fresh content every time
-
-self.addEventListener('install', () => {
-  console.log('[SW] Install - skip waiting');
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  console.log('[SW] Activate - clear caches and claim');
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.map(key => {
-        console.log('[SW] Deleting cache:', key);
-        return caches.delete(key);
-      }));
-    }).then(() => {
-      return self.clients.claim();
-    })
-  );
-});
-
-// Network only - no caching
-self.addEventListener('fetch', event => {
-  // Let all requests go to network
-  return;
-});
+// Empty service worker - no caching
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
